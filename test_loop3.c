fbac8b2e2182ba1a41dc4b27b7e24414a0c89fa7
Yenzy Hebron <yshebron@up.edu.ph>
2023-04-08 00:40:30 +0800

    Modified Makefile to recognize test programs.

--- test_loop3.c
+++ test_loop3.c
@@ -2,18 +2,23 @@
 #include "user.h"
 
 int main() {
-    schedlog(10000);
+    int dummy = 0;
 
-    for (int i = 0; i < 3; i++) {
+    if (priofork(4) == 0) {
+        for (unsigned int i = 0; i < 4e8; i++) {
+            dummy += i;
+        }
         if (fork() == 0) {
             char *argv[] = {"test_loop", 0};
             exec("test_loop", argv);
         }
-    }
 
-    for (int i = 0; i < 3; i++) {
         wait();
-    }
+        
+        exit();
+    } 
 
-    shutdown();
+    wait();
+
+    exit();
 }
